75246c0ca378cd91b27c8799369e3803b6bfd5c4
Filipp Bakanov <filipp@bakanov.su>
2016-04-04 17:05:51 +0300

    Added long description, converting from markdown.

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
 from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
-  name = 'pysyncobj',
-  packages = ['pysyncobj'],
-  version = '0.1.1',
-  description = 'A library for replicating your python class between multiple servers, '
-				'based on raft protocol',
-  author = 'Filipp Ozinov',
-  author_email = 'fippo@mail.ru',
-  url = 'https://github.com/bakwc/PySyncObj',
-  download_url = 'https://github.com/bakwc/PySyncObj/tarball/0.1.1',
-  keywords = ['network', 'replication', 'raft', 'synchronization'],
-  classifiers = [
-	  'Topic :: System :: Networking',
-	  'Topic :: System :: Distributed Computing',
-	  'Intended Audience :: Developers',
-	  'Programming Language :: Python :: 2.7',
-	  'Programming Language :: Python :: 2 :: Only',
-	  'Operating System :: POSIX :: Linux',
-	  'Operating System :: MacOS :: MacOS X',
-	  'License :: OSI Approved :: MIT License',
-  ],
+	name = 'pysyncobj',
+	packages = ['pysyncobj'],
+	version = '0.1.1',
+	description = 'A library for replicating your python class between multiple servers, '
+					'based on raft protocol',
+	long_description=long_description,
+	author = 'Filipp Ozinov',
+	author_email = 'fippo@mail.ru',
+	url = 'https://github.com/bakwc/PySyncObj',
+	download_url = 'https://github.com/bakwc/PySyncObj/tarball/0.1.1',
+	keywords = ['network', 'replication', 'raft', 'synchronization'],
+	classifiers = [
+		'Topic :: System :: Networking',
+		'Topic :: System :: Distributed Computing',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 2 :: Only',
+		'Operating System :: POSIX :: Linux',
+		'Operating System :: MacOS :: MacOS X',
+		'License :: OSI Approved :: MIT License',
+	],
 )
