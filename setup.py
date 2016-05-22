8cb5552bc10dc4d439af35d63d7fb38d41d777c4
Filipp Bakanov <filipp@bakanov.su>
2016-05-22 21:35:00 +0300

    Fixed setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 try:
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
+except(IOError, ImportError, RuntimeError):
     long_description = open('README.md').read()
 
 setup(
