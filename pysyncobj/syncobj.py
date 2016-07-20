45ad980a64fe5b79dacde03ea3effecc2a89f5ce
Filipp Bakanov <filipp@bakanov.su>
2016-07-20 22:07:15 +0300

    Added uptime to debug stats info

--- pysyncobj/syncobj.py
+++ pysyncobj/syncobj.py
@@ -60,6 +60,7 @@ class SyncObj(object):
         self.__forceLogCompaction = False
         self.__leaderCommitIndex = None
         self.__onReadyCalled = False
+        self.__startTime = time.time()
         globalDnsResolver().setTimeouts(self.__conf.dnsCacheTime, self.__conf.dnsFailCacheTime)
         self.__serializer = Serializer(self.__conf.fullDumpFile, self.__conf.logCompactionBatchSize)
         self.__isInitialized = False
@@ -282,6 +283,7 @@ class SyncObj(object):
         LOG_DEBUG('next node idx:', self.__raftNextIndex)
         LOG_DEBUG('match idx:', self.__raftMatchIndex)
         LOG_DEBUG('leader commit idx:', self.__leaderCommitIndex)
+        LOG_DEBUG('uptime:', int(time.time() - self.__startTime))
         LOG_DEBUG('')
 
     def _forceLogCompaction(self):
