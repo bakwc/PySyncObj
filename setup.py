157f5b76bfe6cb99a2e65c2d46f05780b48048ab
Filipp Bakanov <filipp@bakanov.su>
2016-07-23 19:22:27 +0300

    Added python3.5 to categories in setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
         'License :: OSI Approved :: MIT License',
