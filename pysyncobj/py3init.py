c9bd7ef94cb80098d8fc941fae63a7c276afdbcd
Filipp Bakanov <filipp@bakanov.su>
2016-09-17 22:32:32 +0300

    Fixed py3 UT

--- pysyncobj/py3init.py
+++ pysyncobj/py3init.py
@@ -1,2 +1,2 @@
 from syncobj import SyncObj, SyncObjException, SyncObjConf, replicated, replicated_sync, \
-    FAIL_REASON, _COMMAND_TYPE, createJournal, HAS_CRYPTO
+    FAIL_REASON, _COMMAND_TYPE, createJournal, HAS_CRYPTO, SERIALIZER_STATE
--- pysyncobj/pysyncobj3/__init__.py
+++ pysyncobj/pysyncobj3/__init__.py
@@ -2,4 +2,4 @@
 #  WARNING: this is generated file, use generate.sh to update it.
 #
 from .syncobj import SyncObj, SyncObjException, SyncObjConf, replicated, replicated_sync, \
-    FAIL_REASON, _COMMAND_TYPE, createJournal, HAS_CRYPTO
+    FAIL_REASON, _COMMAND_TYPE, createJournal, HAS_CRYPTO, SERIALIZER_STATE
